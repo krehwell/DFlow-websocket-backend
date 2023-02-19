44da59088073755d11729ff0c98fae4da7c697d7
krehwell <fahrimuham001@gmail.com>
2023-02-19 13:13:37 +0700

    able to join a socket

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,24 @@
 import express, { Request, Response } from "express";
 import http from "http";
 import { Server } from "socket.io";
-import type { ISocket } from "./types/common";
+import type { ISocket, IUser } from "./types/common";
 
 const PORT = 5000;
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, { cors: { origin: "*" } });
 
 app.get("/", (_req: Request, res: Response) => {
   res.json({ status: 200, message: "Server is running" });
 });
 
-io.on("connection", (socket: ISocket) => {});
+io.on("connection", (socket: ISocket) => {
+  socket.on("join", ({ username }) => {
+    socket.username = username;
+    console.log("someone joined", username);
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`listening on http://localhost:${PORT}`);
