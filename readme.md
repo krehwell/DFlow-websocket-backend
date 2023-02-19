62e8572aebebcc65ec2abea8a2927916b7cb2903
krehwell <fahrimuham001@gmail.com>
2023-02-19 13:40:37 +0700

    with users tracker handler

--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@ Start server with `yarn dev` and navigate to `localhost:5000`
 
 Features: 
 - Broadcast a message to connected users when someone connects or disconnects ✅
-- Add support for nicknames
+- Add support for username ✅
 - Don’t send the same message to the user that sent it. Instead, append the message directly as soon as he/she presses enter
 - Add “{user} is typing” functionality
 - Support multiple typing tracker
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import http from "http";
 import { Server } from "socket.io";
 import type { ISocket, IUser } from "./types/common";
 
+// SERVER INITIALIZATION
 const PORT = 5000;
 
 const app = express();
@@ -13,14 +14,37 @@ app.get("/", (_req: Request, res: Response) => {
     res.json({ status: 200, message: "Server is running" });
 });
 
+// USERS HANDLER
+class Users {
+    private users: IUser[];
+    constructor() {
+        this.users = [];
+    }
+
+    public addUser(user: IUser) {
+        console.log(user.username, "has joined the chat");
+        this.users.push(user);
+    }
+
+    public removeUser(user: IUser) {
+        console.log(user.username, "has left the chat");
+        this.users = this.users.filter(u => u.id !== user.id);
+    }
+}
+
+const users = new Users();
+
+// SOCKET HANDLER
 io.on("connection", (socket: ISocket) => {
     socket.on("join", ({ username }) => {
         socket.username = username;
-        console.log("someone joined", username);
+        const newUser: IUser = { username, id: socket.id };
+        users.addUser(newUser);
     });
 
     socket.on("disconnect", () => {
-        console.log(`${socket.username} has left the chat`);
+        const user: IUser = { username: socket.username as string, id: socket.id };
+        users.removeUser(user);
     });
 });
 
